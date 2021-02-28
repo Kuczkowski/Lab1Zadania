1e9731e284c9610fb1aba7244fd1c63dd035e972
Kuczkowski <79452575+Kuczkowski@users.noreply.github.com>
2021-02-28 18:21:11 +0100

    Add files via upload

---
+++ Zadanie1.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Zadanie1 {
+    public static void main(String[] args){
+        System.out.print("Ania\n");
+        System.out.print("Bartek\n");
+        System.out.print("Kasia");
+    }
+}
